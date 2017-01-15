1e1b3d12e35f8bef8c74504fcba3423363353c0a
mariano <examinar@examin.ar>
2017-01-15 12:47:39 -0300

    java examples added

---
+++ src/main/java/com/example/streams/ex1/Main.java
@@ -0,0 +1,42 @@
+package com.example.streams.ex1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * streams and lambdas
+ *
+ */
+
+public class Main {
+	public static void main(String[] args) {
+
+		// test lambdas and sum()
+		int[] nums = new int[]{1,2,3};
+		//System.out.println(Arrays.stream(nums).map(x -> x * 2).sum()); // 12
+		
+		
+		// load data
+		Book b1 = new Book("1984","George Orwell", 2);
+		Book b2 = new Book("Animal Farm","George Orwell", 3);
+		Book b3 = new Book("El Silmarillión","Tolkien", 5);
+		
+		// test
+		List<Book> books = new ArrayList<Book>();
+		
+		books.add(b1);
+		books.add(b2);
+		books.add(b3);
+		
+		List<Book> booksFilter = books.stream()
+				.filter(x -> x.getStock() > 3)
+				.collect(Collectors.toList());
+		
+		booksFilter.stream().forEach(x -> {
+			System.out.println(x.getName()); // El Silmarillion
+			System.out.println(x.getStock()); // 5
+		});
+		
+	}
+}
